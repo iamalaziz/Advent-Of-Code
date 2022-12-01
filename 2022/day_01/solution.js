981e7c97cb178a66c6ce32f9e4541fe375214d19
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-12-02 00:29:36 +0900

    part 1 sulution done

--- 2022/day_01/solution.js
+++ 2022/day_01/solution.js
@@ -1,1 +1,24 @@
+const { readFileSync } = require("fs");
 
+function syncReadFile() {
+  const filename =
+    "/Users/abdulaziz/Coding/advent-of-code/years/2022/day_01/input.txt";
+  const contents = readFileSync(filename, "utf-8");
+  const arr = contents.split(/\r?\n/);
+  return arr;
+}
+
+const input = syncReadFile();
+// const input = ["100", "200", "300", "", "400", "500", "", "200", "100", "100"];
+let sum = 0;
+let the_highest_calories = 0;
+
+input.forEach((i) => {
+  if (i !== "") {
+    sum += Number(i);
+  } else {
+    sum > the_highest_calories && (the_highest_calories = sum);
+    sum = 0;
+  }
+});
+console.log(the_highest_calories);
