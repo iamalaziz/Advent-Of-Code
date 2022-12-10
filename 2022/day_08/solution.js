4b7e2384ec6a3f0540ff41d315277b9d135b3231
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-12-10 23:23:30 +0900

    day_08 start

--- 2022/day_08/solution.js
+++ 2022/day_08/solution.js
@@ -2,7 +2,7 @@ const { readFileSync } = require("fs");
 
 function syncReadFile() {
   const filename =
-    "/Users/abdulaziz/Coding/advent-of-code/years/2022/day_07/input.txt";
+    "/Users/abdulaziz/Coding/advent-of-code/years/2022/day_08/input.txt";
   const contents = readFileSync(filename, "utf-8");
   const arr = contents.split(/\r?\n/);
   return arr;
